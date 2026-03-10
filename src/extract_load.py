d8bb5bbdb9eb8d8f85defa39aff231d2e484c58c
Matheus Sabaudo Rodrigues <matteosabaudo@outlook.it>
2026-03-10 22:03:36 +0100

    Data exported to PostgreSQL

--- src/extract_load.py
+++ src/extract_load.py
@@ -7,13 +7,30 @@ import os
 
 # Locals
 
+load_dotenv()
+
 commodities = ['CL=F', 'GC=F', 'SI=F']
 
+DB_HOST = os.getenv('DB_HOST_PROD')
+DB_PORT = os.getenv('DB_PORT_PROD')
+DB_NAME = os.getenv('DB_NAME_PROD')
+DB_USER = os.getenv('DB_USER_PROD')
+DB_PASS = os.getenv('DB_PASS_PROD')
+DB_SCHEMA = os.getenv('DB_SCHEMA_PROD')
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
+
+# API Request
+
 def GetCommodities(symbol, period='5d', interval='1d'):
-    ticker = yf.Ticker('CL=F')
+    ticker = yf.Ticker(symbol)
     dataframe = ticker.history(period=period, interval=interval)[['Close']]
     dataframe['symbol'] = symbol
     return dataframe
+
+# Join commodities
 
 def JoinCommodities(commodities):
     data = []
@@ -22,11 +39,11 @@ def JoinCommodities(commodities):
         data.append(dataframe)
     return pd.concat(data) 
 
+# Connection to DB
+
+def ExportData(df, schema=DB_SCHEMA):
+    df.to_sql('commodities', engine, if_exists='replace', index=True, index_label='Date', schema=schema)
+
 if __name__ == "__main__":
     joinedData = JoinCommodities(commodities)
-
-# API Request
-
-# Join commodities
-
-# Connection to DB
+    ExportData(joinedData, schema='public')
